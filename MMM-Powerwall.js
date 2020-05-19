d94f33d06cd653907b87d0151dac1857e8cb6cd2
Mike Bishop <mbishop@evequefou.be>
2020-05-19 19:47:35 -0400

    Get a graph working, albeit wonky still

--- MMM-Powerwall.js
+++ MMM-Powerwall.js
@@ -7,6 +7,18 @@
  * MIT Licensed.
  */
 
+const DISPLAY_SOURCES = [
+	{ key: "solar", displayAs: "Solar", color: "gold" },
+	{ key: "battery", displayAs: "Powerwall Discharge", color: "green"},
+	{ key: "grid", displayAs: "Grid Import", color: "gray" }
+];
+const DISPLAY_SINKS = [
+	{ key: "house", displayAs: "Local Usage", color: "blue" },
+	{ key: "car", displayAs: "Car Charging", color: "red" },
+	{ key: "battery", displayAs: "Powerwall Charging", color: "green" },
+	{ key: "grid", displayAs: "Grid Export", color: "gray" }
+];
+
 Module.register("MMM-Powerwall", {
 	defaults: {
 		graphs: ["SolarProduction"],
@@ -22,9 +34,11 @@ Module.register("MMM-Powerwall", {
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 	twcEnabled: null,
 	teslaAPIEnabled: null,
-	aggregates: null,
+	teslaAggregates: null,
+	flows: null,
 	historySeries: null,
 	chargingState: null,
+	charts: {},
 
 	start: function() {
 		var self = this;
@@ -80,10 +94,11 @@ Module.register("MMM-Powerwall", {
 
 	getTemplateData: function() {
 		let result = {
+			id: this.identifier,
 			config: this.config,
 			twcEnabled: this.twcEnabled,
 			teslaAPIEnabled: this.teslaAPIEnabled,
-			aggregates: this.aggregates,
+			flows: this.flows,
 			historySeries: this.historySeries,
 			chargingState: this.chargingState,
 		};
@@ -93,8 +108,7 @@ Module.register("MMM-Powerwall", {
 	},
 
 	getScripts: function() {
-		//TODO:  Will need the ChartJS script here
-		return [];
+		return [this.file("node_modules/chart.js/dist/Chart.bundle.js")];
 	},
 
 	getStyles: function () {
@@ -103,27 +117,191 @@ Module.register("MMM-Powerwall", {
 		];
 	},
 
-	createCharts: function () {
-		if( !this.loaded ) {
-			if ( true /* charts have been created */ ) {
-				// Create the charts initially
-				this.loaded = true
-			}
-			else {
-				setTimeout(createCharts, 1000)
-			}
-		}
-	},
-
 	// socketNotificationReceived from helper
 	socketNotificationReceived: function (notification, payload) {
 		Log.log("Received " + notification + ": " + JSON.stringify(payload));
 		if(notification === "MMM-Powerwall-Aggregates") {
 			if( payload.ip === this.config.powerwallIP ) {
-				this.aggregates = payload.aggregates;
-				this.updateDom();
-				// May not need to updateDom; use chartJs to directly modify the graphs	
+				let needUpdate = false;
+				if (!this.flows) {
+					needUpdate = true;
+				}
+
+				this.teslaAggregates = payload.aggregates;
+				this.flows = this.attributeFlows(payload.aggregates, self.twcConsumption);
+				if (needUpdate) {
+					// If we didn't have data before, we need to redraw
+					this.updateDom();
+				}
+				else {
+					// We're updating the data in-place.
+
+					/*******************
+					 * SolarProduction *
+					 *******************/
+					let productionNode = document.getElementById(this.identifier + "-SolarProduction");
+					let solarProduction = this.flows.sources.solar.total;
+					if (solarProduction > 950) {
+						productionNode.innerText = Math.trunc(solarProduction / 100) / 10.0 + " kW";
+					}
+					else {
+						productionNode.innerText = Math.trunc(solarProduction) + " W"
+					}
+				}
 			}
 		}
 	},
+
+	notificationReceived: function(notification, payload, sender) {
+		var self = this;
+		if( !sender ) {
+			// Received from core system
+			if( notification === "MODULE_DOM_CREATED" ) {
+				// DOM has been created -- container will be consistent, but children may be recreated
+				var myDiv = document.getElementById(this.identifier);
+				for( const node of myDiv.children ) {
+					if( node.classList.contains("module-content") ) {
+						// Here's the content div, which we need to observe
+						const config = {attributes: false, childList: true, subtree: false };
+						this.observer = new MutationObserver(function(mutationsList, observer) {self.buildGraphs(); });
+						this.observer.observe(node, config);
+						
+						// Need to trigger it the first time
+						self.buildGraphs()
+						break;
+					}
+					else {
+						Log.log("Found a non-match")
+					}
+				}
+			}
+		}
+	},
+
+	buildGraphs: function() {
+		Log.log("Rebuilding graphs")
+		var myCanvas = document.getElementById(this.identifier + "-SolarDestinations");
+		if( myCanvas ) {
+			let distribution = this.flows.sources.solar.distribution;
+			let toDisplay = DISPLAY_SINKS.filter((entry) => distribution[entry.key] > 0);
+
+			// Build the chart on the canvas
+			var solarProductionPie = new Chart(myCanvas, {
+				type: "doughnut",
+				data: {
+					datasets: [{
+							data: toDisplay.map( (entry) => distribution[entry.key] ),
+							backgroundColor: toDisplay.map( (entry) => entry.color )
+						}],
+					labels: toDisplay.map( (entry) => entry.displayAs ),
+				}
+			});
+			this.charts.solarProduction = solarProductionPie;
+			// self.kerfluffle.foo = 1
+		}
+	},
+	
+	attributeFlows: function(teslaAggregates, twcConsumption) {
+		if( teslaAggregates ) {
+			let solar = Math.trunc(teslaAggregates.solar.instant_power);
+			let battery = Math.trunc(teslaAggregates.battery.instant_power);
+			let house = Math.trunc(teslaAggregates.load.instant_power);
+			let car = 0;
+			if( twcConsumption ) {
+				car = twcConsumption;
+				house -= car;
+			}
+			let grid = teslaAggregates.site.instant_power;
+
+			let flows = {
+				solar: {
+					unassigned: solar,
+					battery: 0,
+					house: 0,
+					car: 0,
+					grid: 0
+				},
+				battery: {
+					unassigned: ((battery > 0) ? battery : 0),
+					house: 0,
+					car: 0,
+					grid: 0
+				},
+				grid: {
+					unassigned: ((grid > 0) ? grid : 0),
+					battery: 0,
+					house: 0,
+					car: 0
+				},
+				unassigned: {
+					battery: ((battery < 0) ? Math.abs(battery) : 0),
+					house: house,
+					car: car,
+					grid: ((grid < 0) ? Math.abs(grid) : 0)
+				}
+			}
+
+			for( const source of ["solar", "battery", "grid"]) {
+				for( const sink of ["battery", "house", "car", "grid"]) {
+					let amount_to_claim = Math.min(flows[source].unassigned, flows.unassigned[sink]);
+					flows[source].unassigned -= amount_to_claim;
+					flows.unassigned[sink] -= amount_to_claim;
+					flows[source][sink] = amount_to_claim;
+				}
+				delete flows[source].unassigned;
+			}
+
+			return {
+				sources: {
+					solar: {
+						total: solar,
+						distribution: flows.solar,
+					},
+					battery: {
+						total: ((battery > 0) ? battery : 0),
+						distribution: flows.battery,
+					},
+					grid: {
+						total: ((grid > 0) ? grid : 0),
+						distribution: flows.grid,
+					}
+				},
+				sinks: {
+					battery: {
+						total: ((battery < 0) ? Math.abs(battery) : 0),
+						sources: {
+							solar: flows.solar.battery,
+							grid: flows.grid.battery
+						}
+					},
+					house: {
+						total: house,
+						sources: {
+							solar: flows.solar.house,
+							battery: flows.battery.house,
+							grid: flows.grid.house
+						}
+					},
+					car: {
+						total: car,
+						sources: {
+							solar: flows.solar.car,
+							battery: flows.battery.car,
+							grid: flows.grid.car
+						}
+					},
+					grid: {
+						total: ((grid < 0) ? Math.abs(grid) : 0),
+						sources: {
+							solar: flows.solar.grid,
+							battery: flows.battery.grid
+						}
+					}
+				}
+			};
+		}
+		else {
+			return null;
+		}
+	}
 });
