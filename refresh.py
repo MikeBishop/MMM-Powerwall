afd9031cc351e5dc7dfc33e06b05fc4b5766fdc4
Mike Bishop <mbishop@evequefou.be>
2021-07-18 20:51:32 -0400

    UA change for refresh as well

--- refresh.py
+++ refresh.py
@@ -8,7 +8,7 @@ import requests
 
 MAX_ATTEMPTS = 7
 CLIENT_ID = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
-UA = "Mozilla/5.0 (Linux; Android 10; Pixel 3 Build/QQ2A.200305.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.81 Mobile Safari/537.36"
+UA = "PostmanRuntime/7.26.10" #"Mozilla/5.0 (Linux; Android 10; Pixel 3 Build/QQ2A.200305.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.81 Mobile Safari/537.36"
 X_TESLA_USER_AGENT = "TeslaApp/3.10.9-433/adff2e065/android/10"
 
 def vprint(*args, **kwargs):
@@ -18,7 +18,7 @@ def refresh(args):
     token = args.token
     session = requests.Session()
 
-    headers = {"user-agent": UA, "x-tesla-user-agent": X_TESLA_USER_AGENT}
+    headers = {"user-agent": UA} #"x-tesla-user-agent": X_TESLA_USER_AGENT}
     payload = {
         "grant_type": 'refresh_token',
         "client_id": 'ownerapi',
