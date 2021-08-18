5692c33e1ae90e447ded6c832f699fa90a65429b
Mike Bishop <mbishop@evequefou.be>
2021-08-18 14:20:25 -0400

    Conditional on success

--- node_helper.js
+++ node_helper.js
@@ -418,7 +418,7 @@ module.exports = NodeHelper.create({
 							ip: powerwallIP,
 							mode: operation.real_mode,
 							reserve: Math.max(
-								Math.round(( operation.backup_reserve_percent - 5 ) / .95),
+								Math.round((operation.backup_reserve_percent - 5) / .95),
 								0
 							)
 						});
@@ -793,11 +793,13 @@ module.exports = NodeHelper.create({
 			let url = "https://owner-api.teslamotors.com/api/1/energy_sites/" + siteID + "/live_status";
 			let cloudStatus = await this.doTeslaApi(url, username, null, siteID, this.storm);
 
-			this.sendSocketNotification("StormWatch", {
-				username: username,
-				siteID: siteID,
-				storm: cloudStatus.storm_mode_active
-			});
+			if (cloudStatus) {
+				this.sendSocketNotification("StormWatch", {
+					username: username,
+					siteID: siteID,
+					storm: cloudStatus.storm_mode_active
+				});
+			}
 		}
 	},
 
